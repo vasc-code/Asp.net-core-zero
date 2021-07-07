533d9a86ca2b46fdaad1ad533ca2fb291ac9c25b
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-07-06 22:48:17 -0300

    curso zero de Asp.NET MVC da Alura

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Alura.ListaLeitura.App.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
